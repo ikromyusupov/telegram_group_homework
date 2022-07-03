6f23ccfe78e143582ffbf37cf7124350ee4f94e6
Ikrom <ikromyusupov09@gmail.com>
2022-07-03 12:06:22 +0500

    number of messages

--- find_number_of_messages.py
+++ find_number_of_messages.py
@@ -10,4 +10,6 @@ def find_number_of_messages(data: dict)->int:
         int: Total number of messages.
     
     """
-    return
+    return len(data['messages'])
+    
+data = read_data("data/result.json")
