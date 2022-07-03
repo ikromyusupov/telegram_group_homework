17bd9a00bec0f7c83a90ab660428ee019f52ee79
Ikrom <ikromyusupov09@gmail.com>
2022-07-03 06:56:09 +0500

    users id

--- find_all_users_id.py
+++ find_all_users_id.py
@@ -9,4 +9,9 @@ def find_all_users_id(data: dict)->list:
     Returns:
         list: List containing all the users id
     """
-    return
+    actor_id = []
+    for i in data['messages']:
+        id1 = i.get('from_id', False)
+        if id1 and id1 not in actor_id:
+            actor_id.append(i.get('from_id'))
+data = read_data("data/result.json")
